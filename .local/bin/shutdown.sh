d733b118047b88f5f804c8228be4a84e698ee1e7
Andy Rufasto <44651310+andyrufasto@users.noreply.github.com>
2020-07-14 13:53:58 -0500

    the linux way de apagar tu pc

---
+++ .local/bin/shutdown.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Import the colors
+. "${HOME}/.cache/wal/colors.sh"
+
+dmenu_color(){ 
+  dmenu -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -p "Shutdown"
+}
+
+election=$(echo "No\nYes" | dmenu_color )
+
+if [ $election = "Yes" ]
+	then systemctl poweroff
+fi
