726c981f16dad437a68de3b9395b38b0b7bd8877
Andy Rufasto <44651310+andyrufasto@users.noreply.github.com>
2020-07-27 23:03:56 -0500

    youtube en mpv

--- .local/bin/mpyt.sh
+++ .local/bin/mpyt.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env sh
-mpv $(xclip -o) > /dev/null 2>&1
+
+mpv "$(xclip -o)" > /dev/null 2>&1 &
+alacritty -t vis -e vis &
+sleep 0.2
+xdotool search --name vis windowactivate &
+xdotool key Super_L+Shift_L+space
+xdotool search --name vis windowsize 1008 138 windowmove 2124 617
+sleep 7
+xdotool search --name mpv windowsize 1011 570 windowmove 2117 25
