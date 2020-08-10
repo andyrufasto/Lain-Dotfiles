d4c9e25d5cd2c9152de7c65ad975c0dbb36f40c0
Andy Rufasto <44651310+andyrufasto@users.noreply.github.com>
2020-08-10 11:39:07 -0500

    mpyt
    script que reproduce el url de un video en youtube copiado en portapapeles, con visualizador y tamaño de mpv personalizado

--- .local/bin/mpyt.sh
+++ .local/bin/mpyt.sh
@@ -4,7 +4,7 @@ mpv "$(xclip -o)" > /dev/null 2>&1 &
 alacritty -t vis -e vis &
 sleep 0.2
 xdotool search --name vis windowactivate &
-xdotool key Super_L+Shift_L+space
+bspc node -t floating
 xdotool search --name vis windowsize 1008 138 windowmove 2124 617
 sleep 7
 xdotool search --name mpv windowsize 1011 570 windowmove 2117 25
