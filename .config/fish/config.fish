26a34c1cbd31d398d97cb45b1390d3041ecff5da
Andy Rufasto <andy@andyrufasto.cf>
2020-11-01 12:46:52 -0500

    fish vi mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,11 @@
 neofecth
+
 abbr r "ranger"
 abbr glo "git log --oneline -10"
-
+abbr gc "git commit -S"
 source /home/lain/.cache/wal/colors.fish
 
 set fish_greeting
+
+fish_vi_key_bindings
+# fish_default_key_binding
