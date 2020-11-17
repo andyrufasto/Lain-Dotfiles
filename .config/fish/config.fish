0fbedcc687d5846e3b1af14bac28bf552b78a399
Andy Rufasto <andy@andyrufasto.cf>
2020-11-17 14:53:48 -0500

    config fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,9 +3,9 @@ neofetch
 abbr r "ranger"
 abbr glo "git log --oneline -10"
 abbr gc "git commit -S"
-source /home/lain/.cache/wal/colors.fish
+abbr lg "lazygit"
 
 set fish_greeting
-
 fish_vi_key_bindings
 # fish_default_key_binding
+source /home/lain/.cache/wal/colors.fish
