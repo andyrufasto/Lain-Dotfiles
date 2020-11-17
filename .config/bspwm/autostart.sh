d37004d1b7a4458fd864a6d1302476494a200aad
Andy Rufasto <andy@andyrufasto.cf>
2020-11-17 14:50:11 -0500

    bspwm config

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -8,20 +8,20 @@ function run {
 }
 
 #xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-xrandr --output HDMI1 --mode 1920x1080
+xrandr --output HDMI1 --mode 1920x1080 
 xrandr --output HDMI1 --primary --left-of DP1
-$HOME/.config/polybar/launch.sh &
 
 #change your keyboard if you need it
 #setxkbmap -layout be
-
+~/.local/bin/pywal ~/Pictures/wallpaper.jpg &
+$HOME/.config/polybar/launch.sh &
+dunst &
 mpd &
-~/.local/bin/pywal ~/Pictures/wallpaper/cat_green.jpeg &
 
 #Some ways to set your wallpaper besides variety or nitrogen
 #feh --bg-scale ~/.config/bspwm/wall.png &
 
-dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
+dex $HOME/.config/autostart/arcolinux-welcome-app.desktop &
 xsetroot -cursor_name left_ptr &
 run sxhkd -c ~/.config/sxhkd/sxhkdrc &
 
@@ -32,7 +32,7 @@ numlockx on &
 blueberry-tray &
 picom --config $HOME/.config/picom/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-#/usr/lib/xfce4/notifyd/xfce4-notifyd &
-dunst &
+# /usr/lib/xfce4/notifyd/xfce4-notifyd &
+eval "$(ssh-agent -s)" &
 run volumeicon &
 
