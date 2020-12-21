2608a57f34c7ff3a114faf9ead1c2c3978225319
Andy Rufasto <andy@andyrufasto.cf>
2020-12-21 14:18:06 -0500

    experimental backend

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -30,7 +30,7 @@ run pamac-tray &
 run xfce4-power-manager &
 numlockx on &
 blueberry-tray &
-picom --config $HOME/.config/picom/picom.conf &
+picom --config $HOME/.config/picom/picom.conf --experimental-backends &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 # /usr/lib/xfce4/notifyd/xfce4-notifyd &
 eval "$(ssh-agent -s)" &
