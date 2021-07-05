87d34faed0e331d07868943e91c4e01100f96227
Andy Rufasto <andy@andyrufasto.cf>
2021-07-04 20:06:07 -0500

    getpath function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,8 @@ abbr r "ranger"
 abbr glo "git log --oneline -10"
 abbr gc "git commit -S -m"
 abbr lg "lazygit"
+abbr getpath "find -type f | fzf | sed 's/^..//' | tr -d '\n' | xclip -sel c"
+
 
 set fish_greeting
 fish_vi_key_bindings
